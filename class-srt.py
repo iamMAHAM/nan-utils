81606c104a146ad7e77c237d198bc86781fa04eb
iamMAHAM <abdulkabore@gmail.com>
2023-05-12 15:32:20 +0000

    rename englist srt with right name

---
+++ class-srt.py
@@ -0,0 +1,11 @@
+from functions import scan_dirs, define_courses_path
+
+courses = scan_dirs(define_courses_path())  # ['Py', 'JS', 'PHP']
+
+for course in courses:
+    for section in course.iterdir():
+        for file in section.iterdir():
+            if file.suffix == '.srt' and not file.stem.endswith('_en'):
+                print(file.stem, file.suffix)
+                f = file.rename(f'{file.parent}/{file.stem}_en.srt')
+                print(f)
